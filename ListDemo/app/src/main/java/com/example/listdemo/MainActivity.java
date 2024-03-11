7c44ff25742b347722bdfc19c4fc4a1a5fb93541
Tronganh1611 <tronganh16112003@gmail.com>
2024-03-11 16:53:27 +0700

    Làm phần thêm mới cho listview

--- ListDemo/app/src/main/java/com/example/listdemo/MainActivity.java
+++ ListDemo/app/src/main/java/com/example/listdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> dsTenTinhThanhVN;//khai bao
+    ArrayAdapter<String> adapterTinhThanh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         //Hiển thị dữ liệu trên list view
-
+        Button nut= findViewById(R.id.btn);
         //B1+:cần có dữ liệu
 
         dsTenTinhThanhVN = new ArrayList<String>();//tao the hien cu the
@@ -38,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
         dsTenTinhThanhVN.add("Phú Yên");
         dsTenTinhThanhVN.add("Cà Mau");
         //Buoc 2 tao Adapter
-        ArrayAdapter<String> adapterTinhThanh;
         adapterTinhThanh = new ArrayAdapter<String>(this,
                                                             android.R.layout.simple_list_item_1
                                                             ,dsTenTinhThanhVN);
@@ -49,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         lvTenTinhThanh.setAdapter(adapterTinhThanh);
         //3.3 gan bo lang nghe
         lvTenTinhThanh.setOnItemClickListener(BoLangNghevaXL);
+        nut.setOnClickListener(Them);
     }
     //Tao bo lang nghe va xu li su kien Onclick
     //Vd:BolangNgheVaXL
@@ -59,6 +62,20 @@ public class MainActivity extends AppCompatActivity {
             //hien thong bao vi tri khi chon
             String strTEnTinhTHhanh = dsTenTinhThanhVN.get(position);
             Toast.makeText(MainActivity.this,strTEnTinhTHhanh , Toast.LENGTH_SHORT).show();
+        }
+    };
+    View.OnClickListener Them = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText strTP = findViewById(R.id.edt);
+
+            String Ten = strTP.getText().toString();
+            if (!Ten.isEmpty()){
+                dsTenTinhThanhVN.add(Ten);
+                adapterTinhThanh.notifyDataSetChanged();
+            }else{
+                Toast.makeText(MainActivity.this, "Tên chưa được nhập mời bạn nhập lại ", Toast.LENGTH_SHORT).show();
+            }
         }
     };
 }
