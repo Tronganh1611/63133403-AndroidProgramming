97d2b5ef76be5c6adaa47ee81ed19ecab73be9fd
Tronganh1611 <tronganh16112003@gmail.com>
2024-03-26 02:55:41 +0700

    Hoàn thành câu 1 với câu 3

---
+++ Cau1_AppBMI/src/ntu/Anh_63133403/ChuongTrinhBMI.java
@@ -0,0 +1,11 @@
+package ntu.Anh_63133403;
+
+public class ChuongTrinhBMI {
+
+	public static void main(String[] args) {
+		//Tạo một màn hình GUI để tính toán
+		new Cau1_AppBMI().setVisible(true);
+
+	}
+
+}
