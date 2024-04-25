f13b3dbc84d4f04df193c82665784fb9e1315c44
Tronganh1611 <tronganh16112003@gmail.com>
2024-04-26 00:56:17 +0700

    Hoàn Thành vd2 Intent

--- VD2Intent/app/src/main/java/ntu/anh/vd2intent/MainActivity.java
+++ VD2Intent/app/src/main/java/ntu/anh/vd2intent/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.anh.vd2intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+    }
+    public  void DangNhap(View v){
+        //tạo đói tượng tham số thứ 2 là màn hình ta muoons chuyển sang
+        Intent iManHinhKhac = new Intent(this, MainDangNhap.class);
+        //thực hiện chuyển
+        startActivity(iManHinhKhac);
     }
 }
--- VD2Intent/app/src/main/java/ntu/anh/vd2intent/MainXacNhan.java
+++ VD2Intent/app/src/main/java/ntu/anh/vd2intent/MainXacNhan.java
@@ -1,6 +1,10 @@
 package ntu.anh.vd2intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,11 @@ public class MainXacNhan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_xacnhan);
+        String username = getIntent().getStringExtra("username");
+        EditText StrA = findViewById(R.id.tvUserName);
+        StrA.setText(username);
     }
+
+
+
 }
