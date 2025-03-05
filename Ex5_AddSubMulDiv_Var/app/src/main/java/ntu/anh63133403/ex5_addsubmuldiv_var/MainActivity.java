4208658f2c65ff110bc82d633e0740be7cae2bcf
Tronganh1611 <tronganh16112003@gmail.com>
2025-03-05 14:19:56 +0700

    hoan thanh ex5_Var

--- Ex5_AddSubMulDiv_Var/app/src/main/java/ntu/anh63133403/ex5_addsubmuldiv_var/MainActivity.java
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/ntu/anh63133403/ex5_addsubmuldiv_var/MainActivity.java
@@ -2,6 +2,12 @@ package ntu.anh63133403.ex5_addsubmuldiv_var;
 
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,7 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editso1;
+    EditText editso2;
+    EditText editkq;
+    Button nutCong ,nutTru, nutNhan, nutChia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,112 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Timdieukhien();
+        nutCong.setOnClickListener(Bo_xuliCong);
+        nutTru.setOnClickListener(Bo_xuliTru);
+        nutNhan.setOnClickListener(Bo_xuliNhan);
+        nutChia.setOnClickListener(Bo_xuliChia);
     }
+    View.OnClickListener Bo_xuliCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            XuliCong(view);
+        }
+    };
+    View.OnClickListener Bo_xuliTru = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            XuliTru(view);
+        }
+    };
+    View.OnClickListener Bo_xuliNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            XuliNhan(view);
+        }
+    };
+    View.OnClickListener Bo_xuliChia = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            XuliChia(view);
+        }
+    };
+    void Timdieukhien(){
+        editso1 = (EditText) findViewById(R.id.edtSo1);
+        editso2 = (EditText) findViewById(R.id.edtSo2);
+        editkq = (EditText) findViewById(R.id.edtkq);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+    }
+    public void XuliCong(View v){
+
+        //lay du lieu
+        String soThu1 = editso1.getText().toString();
+        String soThu2 = editso2.getText().toString();
+        //chuyen du lieu
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tinh
+        float tong = soA + soB;
+        //hien kq
+
+        //xuat kq
+        String kq = String.valueOf(tong);
+        //
+        editkq.setText(kq);
+    }
+    public void XuliTru(View v){
+
+        //lay du lieu
+        String soThu1 = editso1.getText().toString();
+        String soThu2 = editso2.getText().toString();
+        //chuyen du lieu
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tinh
+        float Hieu = soA - soB;
+        //hien kq
+
+        //xuat kq
+        String kq = String.valueOf(Hieu);
+        //
+        editkq.setText(kq);
+    }
+    public void XuliNhan(View v){
+
+        //lay du lieu
+        String soThu1 = editso1.getText().toString();
+        String soThu2 = editso2.getText().toString();
+        //chuyen du lieu
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tinh
+        float tich = soA * soB;
+        //hien kq
+
+        //xuat kq
+        String kq = String.valueOf(tich);
+        //
+        editkq.setText(kq);
+    }
+    public void XuliChia(View v){
+
+        //lay du lieu
+        String soThu1 = editso1.getText().toString();
+        String soThu2 = editso2.getText().toString();
+        //chuyen du lieu
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tinh
+        float thuong = soA / soB;
+        //hien kq
+
+        //xuat kq
+        String kq = String.valueOf(thuong);
+        //
+        editkq.setText(kq);
+    }
+
 }
