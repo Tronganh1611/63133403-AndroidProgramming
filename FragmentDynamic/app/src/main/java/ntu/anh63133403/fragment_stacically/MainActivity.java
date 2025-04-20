a3db66dacfa48c5bbc1f71ed18109ce9bf7c4224
Tronganh1611 <tronganh16112003@gmail.com>
2025-04-20 23:51:42 +0700

    hoan thanh fragment

---
+++ FragmentDynamic/app/src/main/java/ntu/anh63133403/fragment_stacically/MainActivity.java
@@ -0,0 +1,30 @@
+package ntu.anh63133403.fragment_stacically;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView3, new ContentFragment())
+                .add(R.id.fragmentContainerView4, new FooterFragment())
+                .commit();
+    }
+}
